946f8f81f3d6116e600902a28d9810021e07b765
Adondokov <145752531+Adondokov@users.noreply.github.com>
2024-04-09 15:58:12 +0700

    Add files via upload

---
+++ task2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <omp.h>
+
+double f(double x) {
+    return x * x;
+}
+
+double integrate_omp(double a, double b, int nsteps) {
+    double sum = 0.0;
+    double step = (b - a) / nsteps;
+
+    #pragma omp parallel for reduction(+:sum)
+    for (int i = 0; i < nsteps; ++i) {
+        double x = a + (i + 0.5) * step;
+        double local_sum = f(x) * step;
+        #pragma omp atomic
+        sum += local_sum;
+    }
+
+    return sum;
+}
+
+int main() {
+    const int nsteps = 40000000;
+    const double a = 0.0;
+    const double b = 1.0;
+
+    std::cout << "Number of Steps: " << nsteps << std::endl;
+    std::cout << "-----------------------------------" << std::endl;
+    std::cout << "Number of Threads | Time (seconds)" << std::endl;
+
+    for (int num_threads : {1, 2, 4, 7, 8, 16, 20, 40}) {
+        omp_set_num_threads(num_threads);
+        double start_time = omp_get_wtime();
+        double result = integrate_omp(a, b, nsteps);
+        double end_time = omp_get_wtime();
+        std::cout << "       " << num_threads << "        |     " << end_time - start_time << std::endl;
+    }
+
+    return 0;
+}
